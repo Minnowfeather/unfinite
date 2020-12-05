c8c77b90e6dfbd2caee21fa2f0c9e4aa93bb6580
Minnowfeather <35814311+Minnowfeather@users.noreply.github.com>
2020-12-05 00:40:13 -0800

    Basic bullet movement

---
+++ Unfinite/Assets/Scripts/vectorMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vectorMove : MonoBehaviour
+{
+    private float dx, dy, angle;
+    private Rigidbody2D body;
+    Vector3 v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dx = 0.0f;
+        dy = 0.0f;
+        angle = 0.0f;
+        body = GetComponent<Rigidbody2D>();
+        v = new Vector3(0.0f,0.0f, 0.0f);
+        addVelocity(0.05f, Mathf.PI);
+    }
+
+    void addVelocity(float magnitude, float a)
+    {
+        dx = magnitude * Mathf.Cos(a);
+        dy = magnitude * Mathf.Sin(a);
+        v = new Vector3(dx,dy, 0.0f);
+        angle = a;
+        // body.RotateAngle(a);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.MovePosition(transform.position + v);
+        
+    }
+}
