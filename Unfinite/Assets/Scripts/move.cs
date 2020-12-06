0ba453cf4af461f5b9472ad65319128ca5bd91a6
Conbonbot <conbonbot745@gmail.com>
2020-12-05 23:19:01 -0800

    Added movement

---
+++ Unfinite/Assets/Scripts/move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 15f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() 
+    {
+        rb.velocity = new Vector3(speed * Input.GetAxis("Horizontal"), speed * Input.GetAxis("Vertical"), 0f);
+    }
+}
