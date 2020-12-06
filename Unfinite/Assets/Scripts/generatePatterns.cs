b9dade2d18dbd666c9c26370163ba0086ef324f7
Minnowfeather <35814311+Minnowfeather@users.noreply.github.com>
2020-12-05 19:40:42 -0800

    Started cone pattern

--- Unfinite/Assets/Scripts/generatePatterns.cs
+++ Unfinite/Assets/Scripts/generatePatterns.cs
@@ -7,14 +7,59 @@ public class generatePatterns : MonoBehaviour
 {
 
     public GameObject prefab;
+    private bool shant = false;
+    private float dT = 0; 
+    private float sInterval;
+    private float sSpeed;
+    private int sAmount;
+    private int sCount = 0;
+    private float sAngle = 0.0f;
+    private bool sUp = true;
+    private bool poolGenerated = false;
+    private GameObject sBoss;
+    private List<GameObject> pool;
+    private float sOffset;
 
     void Start()
     {
-
+        pool = new List<GameObject>();
     }
     void Update()
     {
-
+        if(shant){
+            // LUKAS: PLEASE HELP ME OH GOD WHAT IS THIS
+            while(!poolGenerated){
+                pool.Add(Instantiate(prefab, sBoss.transform.position, new Quaternion(0,0,0,0), sBoss.transform));
+                if(sAmount >= sCount){
+                    poolGenerated = true;
+                    sAmount = 0;
+                }
+            }
+            dT += Time.deltaTime;
+            if(dT >= (sInterval/1000.0f)){
+                if(sAngle <= Mathf.PI/12.0f && sUp){
+                    sAngle += Mathf.PI/60.0f;
+                } else {
+                    sUp = false;
+                    if(sAngle >= -Mathf.PI/12.0f){
+                        sAngle -= Mathf.PI/60.0f;
+                    } else {
+                        sUp = true;
+                    }
+                }
+                print(sAngle);
+                pool[0].GetComponent<vectorMove>().addVelocity(sSpeed, sAngle + sOffset);
+                pool.RemoveAt(0);
+                sAmount++;
+                dT = 0;
+            }
+            if(sCount >= sAmount){
+                sCount = 0;
+                shant = false;
+                poolGenerated = false;
+            }
+        }
+        
     }
     public void circle(GameObject boss, int quantity, float speed)
     {
@@ -33,26 +78,19 @@ public class generatePatterns : MonoBehaviour
         }
     }
 
-    public void shantsStupidCone(GameObject boss, float time, float speed){
-        print("peepee");
-        float angle = 0.0f;
-        bool up = true;
-        for(int i = 0; i < time; i++){
-            GameObject temp = Instantiate(prefab, boss.transform.position, new Quaternion(0,0,0,0), boss.transform);
-            if(angle < time/10 && up){
-                angle++;
-            } else {
-                up = false;
-                if(angle > -time/10){
-                    angle--;
-                } else {
-                    up = true;
-                }
-            }
-            temp.GetComponent<vectorMove>().addVelocity(speed, angle);
-        }
+    public void startCone(GameObject boss, int amount, float interval, float speed, float offset){
+        shant = true;
+        sBoss = boss;
+        sInterval = interval;
+        sSpeed = speed;
+        sAmount = amount;
+        sOffset = offset;
         // maximum pi/12 radians up pi/12 down
         //
+    }
+
+    private void coneNext(){
+
     }
 
 }
--- Unfinite/Assets/Scripts/testFire.cs
+++ Unfinite/Assets/Scripts/testFire.cs
@@ -7,9 +7,10 @@ public class testFire : MonoBehaviour
     private generatePatterns p;
     private GameObject g;
     float c = 0;
-    public int things = 0;
+    public int amount = 0;
     public float speed = 0.0f;
     public float dR = 0.0f;
+    public float interval = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,7 @@ public class testFire : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown("[1]") || Input.GetKeyDown("[5]")){
-            p.shantsStupidCone(g, things, speed);
+            p.startCone(g, amount, interval, speed);
             c += dR;
         }
     }
