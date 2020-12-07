b461153bfb9ba1ad859ebcd7d70c26ae40e69293
Minnowfeather <35814311+Minnowfeather@users.noreply.github.com>
2020-12-06 18:33:23 -0800

    Completed cone/laser; Debug tools updated; Added basic documentation for my methods

--- Unfinite/Assets/Scripts/generatePatterns.cs
+++ Unfinite/Assets/Scripts/generatePatterns.cs
@@ -6,91 +6,298 @@ using UnityEditor;
 public class generatePatterns : MonoBehaviour
 {
 
-    public GameObject prefab;
-    private bool shant = false;
+    public GameObject bulletPrefab;
+    public GameObject laserPrefab;
     private float dT = 0; 
-    private float sInterval;
-    private float sSpeed;
-    private int sAmount;
-    private int sCount = 0;
-    private float sAngle = 0.0f;
-    private bool sUp = true;
-    private bool poolGenerated = false;
-    private GameObject sBoss;
+    private GameObject bulletBoss;
+
+    // cone specific
+    private bool cone_active = false;
+    private float cone_Interval, cone_Speed, cone_Offset, cone_rotationSpeed, cone_spread;
+    private int cone_Amount;
+    private int cone_Count = 0;
+    private float cone_Angle = 0.0f;
+    private bool cone_Up = true;
+
+    // laser specific
+    private bool laser_active = false;
+    private int laser_quantity;
+    private float laser_spawnTime, laser_rotationSpeed, laser_offset, laser_duration;
+    private List<GameObject> activeLasers;
+
+    // pool stuf
     private List<GameObject> pool;
-    private float sOffset;
+    private bool poolGenerated = false;
 
     void Start()
     {
         pool = new List<GameObject>();
+        activeLasers = new List<GameObject>();
     }
     void Update()
     {
-        if(shant){
+        if(cone_active){
             // LUKAS: PLEASE HELP ME OH GOD WHAT IS THIS
-            while(!poolGenerated){
-                pool.Add(Instantiate(prefab, sBoss.transform.position, new Quaternion(0,0,0,0), sBoss.transform));
-                if(sAmount >= sCount){
+            while(!poolGenerated && cone_Amount != 0){
+                pool.Add(Instantiate(bulletPrefab, bulletBoss.transform.position, new Quaternion(0,0,0,0), bulletBoss.transform));
+                cone_Count++;
+                if(cone_Count >= cone_Amount){
                     poolGenerated = true;
-                    sAmount = 0;
+                    cone_Count = 0;;
                 }
             }
             dT += Time.deltaTime;
-            if(dT >= (sInterval/1000.0f)){
-                if(sAngle <= Mathf.PI/12.0f && sUp){
-                    sAngle += Mathf.PI/60.0f;
+            if(dT >= (cone_Interval/1000.0f)){
+                if(cone_Angle <= cone_spread && cone_Up){
+                    cone_Angle += (cone_spread*(1/cone_rotationSpeed));
                 } else {
-                    sUp = false;
-                    if(sAngle >= -Mathf.PI/12.0f){
-                        sAngle -= Mathf.PI/60.0f;
+                    cone_Up = false;
+                    if(cone_Angle >= -cone_spread){
+                        cone_Angle -= (cone_spread*(1/cone_rotationSpeed));
                     } else {
-                        sUp = true;
+                        cone_Up = true;
                     }
                 }
-                print(sAngle);
-                pool[0].GetComponent<vectorMove>().addVelocity(sSpeed, sAngle + sOffset);
+                if(cone_Amount == 0){
+                    pool.Add(Instantiate(bulletPrefab, bulletBoss.transform.position, new Quaternion(0,0,0,0), bulletBoss.transform));
+                }
+                print(cone_Offset);
+                pool[0].GetComponent<vectorMove>().addVelocity(cone_Speed, cone_Angle + cone_Offset);
                 pool.RemoveAt(0);
-                sAmount++;
-                dT = 0;
+                if(cone_Amount != 0){
+                    cone_Count++;
+                }
+                dT = dT - cone_Interval/1000.0f;
             }
-            if(sCount >= sAmount){
-                sCount = 0;
-                shant = false;
+            if(cone_Count >= cone_Amount && cone_Amount != 0){
+                cone_Count = 0;
+                cone_active = false;
                 poolGenerated = false;
             }
         }
+        // if(laser){
+            
+        // }
         
     }
+
+    /*
+
+        Circle
+
+        @param boss
+            The boss that the bullets attach themselves to.
+            Bullets spawn on the boss and extend outwards.
+
+        @param quantity
+            The amount of bullets to be made.
+            Bullets are distributed evenly around a circle.
+
+        @param speed
+            The speed of the bullets.
+
+        @param aR
+            The angle at which the laser starts.
+            Default (0) is an angle of 0 degrees pointing straight right.
+
+    */
     public void circle(GameObject boss, int quantity, float speed)
     {
         float dR = (2*Mathf.PI) / quantity;
         for(int i = 0; i < quantity; i++){
-            GameObject temp = Instantiate(prefab, boss.transform.position, new Quaternion(0,0,0,0), boss.transform);
+            GameObject temp = Instantiate(bulletPrefab, boss.transform.position, new Quaternion(0,0,0,0), boss.transform);
             temp.GetComponent<vectorMove>().addVelocity(speed, i*dR);
         }
     }
-    public void circle(GameObject boss, int quantity, float speed, float aR)
+    public void circle(GameObject boss, int quantity, float speed, float offset)
     {
         float dR = (2*Mathf.PI) / quantity;
         for(int i = 0; i < quantity; i++){
-            GameObject temp = Instantiate(prefab, boss.transform.position, new Quaternion(0,0,0,0), boss.transform);
-            temp.GetComponent<vectorMove>().addVelocity(speed, (i*dR)+aR);
+            GameObject temp = Instantiate(bulletPrefab, boss.transform.position, new Quaternion(0,0,0,0), boss.transform);
+            temp.GetComponent<vectorMove>().addVelocity(speed, (i*dR)+offset);
         }
     }
 
-    public void startCone(GameObject boss, int amount, float interval, float speed, float offset){
-        shant = true;
-        sBoss = boss;
-        sInterval = interval;
-        sSpeed = speed;
-        sAmount = amount;
-        sOffset = offset;
+    /*
+
+        Cone
+
+        @param boss
+            This is the boss it spawns on.
+            The bullets start on the boss and extend outwards
+        
+        @param quantity
+            Number of bullets to be shot in total.
+
+        @param interval
+            How long (in miliseconds) it takes to spawn a bullet.
+
+        @float speed
+            The speed of the bullets.
+
+        @param offset
+            The angle at which the cone starts.
+            Default (0) is an angle of 0 degrees pointing straight right.
+
+        @param rotationSpeed
+            How fast the cone rotates.
+            Higher values mean slower rotations.
+
+        @param spread
+            The maximum angle up/down from the default axis that the bullets will travel in.
+            
+        Functions should be self-explanatory
+
+    */
+    public void cone(GameObject boss, int quantity, float interval, float speed, float offset, float rotationSpeed, float spread){
+        cone_active = true;
+        bulletBoss = boss;
+        cone_Amount = quantity;
+        cone_Interval = interval;
+        cone_Speed = speed;
+        cone_Offset = offset;
+        cone_rotationSpeed = rotationSpeed;
+        cone_spread = spread;
         // maximum pi/12 radians up pi/12 down
-        //
+    }
+    public void startCone(GameObject boss, float interval, float speed, float offset, float rotationSpeed, float spread){
+        if(!cone_active){
+            cone_active = true;
+            bulletBoss = boss;
+            cone_Amount = 0;
+            cone_Interval = interval;
+            cone_Speed = speed;
+            cone_Offset = offset;
+            cone_rotationSpeed = rotationSpeed;
+            cone_spread = spread;
+        }
+        // maximum pi/12 radians up pi/12 down
+    }
+    public void stopCone(){
+        if(cone_active){
+            cone_active = false;
+        }
+    }
+    public void setConeOffset(float offset){
+        cone_Offset = offset;
+    }
+    public void setConeInterval(float interval){
+        cone_Interval = interval;
+    }
+    public void setConeSpeed(float speed){
+        cone_Speed = speed;
+    }
+    public void setConeRotationSpeed(float rotationSpeed){
+        cone_rotationSpeed = rotationSpeed;
+    }
+    public void setConeSpread(float spread){
+        cone_spread = spread;
     }
 
-    private void coneNext(){
+    /*
 
+        Laser
+
+        @param boss
+            This is the boss it spawns on.
+            The lasers anchor on the boss and extend outwards
+        
+        @param quantity
+            Number of lasers.
+            Distributed evently across a circle.
+
+        @oaram spawnTime
+            The time it takes for the laser to become active (have hurtbox).
+
+        @param rotationSpeed
+            How fast the laser rotates.
+            Measured in radians per frame.
+            Higher values mean faster rotations
+
+        @param offset
+            The angle at which the laser starts.
+            Default (0) is an angle of 0 degrees pointing straight right.
+
+        @param length
+            The length multiplier of the laser
+            Final length = (base length) * (length)
+            
+        Functions should be self-explanatory
+
+    */
+    public void laser(GameObject boss, int quantity, float spawnTime, float rotationSpeed, float offset, float length)
+    {
+        laser_active = true;
+        bulletBoss = boss;
+        laser_quantity = quantity;
+        laser_spawnTime = spawnTime;
+        laser_rotationSpeed = rotationSpeed;
+        laser_offset = offset;
+        float dR = (2*Mathf.PI) / laser_quantity;
+        for(int i = 0; i < laser_quantity; i++){
+            GameObject temp = Instantiate(laserPrefab, bulletBoss.transform.position, new Quaternion(0,0,0,0), bulletBoss.transform);
+            temp.transform.localScale = new Vector3(length, 1, 1);
+            temp.GetComponent<laserRotate>().setRotationRadians(1);
+            temp.GetComponent<laserRotate>().turnSpeed = laser_rotationSpeed*Mathf.Rad2Deg;
+            temp.GetComponent<laserRotate>().enableFreeRotate(true);
+        }
     }
+    public void startLaser(GameObject boss, int quantity, float spawnTime, float rotationSpeed, float offset, float length)
+    {
+        if(!laser_active){
+            laser_active = true;
+            bulletBoss = boss;
+            laser_quantity = quantity;
+            laser_spawnTime = spawnTime;
+            laser_rotationSpeed = rotationSpeed;
+            laser_offset = offset;
+            float dR = (2*Mathf.PI) / laser_quantity;
+            for(int i = 0; i < laser_quantity; i++){
+                GameObject temp = Instantiate(laserPrefab, bulletBoss.transform.position, new Quaternion(0,0,0, 0), bulletBoss.transform);
+                temp.transform.localScale = new Vector3(length, 1, 1);
+                temp.transform.Rotate(0, 0, ((i*dR)+laser_offset) * Mathf.Rad2Deg);
+                temp.GetComponent<laserRotate>().setRotationRadians(1);
+                temp.GetComponent<laserRotate>().turnSpeed = laser_rotationSpeed*Mathf.Rad2Deg;
+                temp.GetComponent<laserRotate>().enableFreeRotate(true);
+                activeLasers.Add(temp);
+            }
+        }
+    }
+    public void stopLaser()
+    {
+        if(laser_active){
+            laser_active = false;
+            for(int i = 0; i < laser_quantity; i++){
+                Destroy(activeLasers[i]);
+            }
+            activeLasers = new List<GameObject>();
+        }
+    }
+    public void pauseLaser()
+    {
+        if(laser_active){
+            for(int i = 0; i < laser_quantity; i++){
+                activeLasers[i].GetComponent<laserRotate>().stopRotating();
+            }
+        }
+    }
+    public void resumeLaser()
+    {
+        if(laser_active){
+            for(int i = 0; i < laser_quantity; i++){
+                activeLasers[i].GetComponent<laserRotate>().enableFreeRotate(true);
+            }
+        }
+    }
+    public void setLaserOffset(float offset){
+        laser_offset = offset;
+    }
+    public void setLaserRotationSpeed(float rotationSpeed){
+        laser_rotationSpeed = rotationSpeed;
+        for(int i = 0; i < laser_quantity; i++){
+            activeLasers[i].GetComponent<laserRotate>().turnSpeed = laser_rotationSpeed*Mathf.Rad2Deg;
+        }
+    }
+
 
 }
--- Unfinite/Assets/Scripts/testFire.cs
+++ Unfinite/Assets/Scripts/testFire.cs
@@ -6,11 +6,13 @@ public class testFire : MonoBehaviour
 {
     private generatePatterns p;
     private GameObject g;
-    float c = 0;
     public int amount = 0;
     public float speed = 0.0f;
     public float dR = 0.0f;
     public float interval = 0.0f;
+    public float rotationSpeed = 0.0f;
+    public float spread = Mathf.PI/12.0f;
+    public bool circle, cone, laser = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +23,35 @@ public class testFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("[1]") || Input.GetKeyDown("[5]")){
-            p.startCone(g, amount, interval, speed, 0);
-            c += dR;
+        if(Input.GetKeyDown("[1]")){
+            if(cone){
+                p.startCone(g, interval, speed, dR, rotationSpeed, spread);
+            }
+            if(circle){
+                p.circle(g, amount, speed, dR);
+            }
+            if(laser){
+                p.startLaser(g, amount, 0, rotationSpeed, dR, 3);
+            }
         }
+        if(Input.GetKeyDown("[3]")){
+            p.stopCone();
+            if(laser){
+                p.stopLaser();
+            }
+        }
+        if(Input.GetKeyDown("[2]")){
+            p.pauseLaser();
+        }
+        if(Input.GetKeyDown("[5]")){
+            p.resumeLaser();
+        }
+        p.setConeOffset(dR);
+        p.setConeInterval(interval);
+        p.setConeSpeed(speed);
+        p.setConeRotationSpeed(rotationSpeed);
+        p.setConeSpread(spread);
+        p.setLaserOffset(dR);
+        p.setLaserRotationSpeed(rotationSpeed);
     }
 }
