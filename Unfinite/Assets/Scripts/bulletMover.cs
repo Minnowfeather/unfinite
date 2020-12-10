2446fa88eed8146f1bbbe23b554d244c9eed337e
Conbonbot <conbonbot745@gmail.com>
2020-12-09 21:59:24 -0800

    commented code

--- Unfinite/Assets/Scripts/bulletMover.cs
+++ Unfinite/Assets/Scripts/bulletMover.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class bulletMover : MonoBehaviour
 {
-    public Rigidbody2D clone;
-    public Vector3 input_position;
+    public Rigidbody2D bullet; // The prefab of the bullet
+    public Vector3 input_position; // The position of the player when the bullet is fired
 
     void Start()
     {
-        clone = GetComponent<Rigidbody2D>();
-        Vector3 vec = -(input_position - Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        vec.z = 0;
-        clone.velocity = vec;
+        bullet = GetComponent<Rigidbody2D>();                                                   // Grabs the 2D Rigidbody component
+        Vector3 vec = -(input_position - Camera.main.ScreenToWorldPoint(Input.mousePosition));  // Creates a vector that goes from the player's position to the mouse's position
+        vec.z = 0;              // sets the z value of that vector to be zero (we are in 2d here hehe)
+        bullet.velocity = vec;  // sets the bullets velocity equal to that vector (bullet go WEEEEE)     
     }
 
     void Update()
--- Unfinite/Assets/Scripts/move.cs
+++ Unfinite/Assets/Scripts/move.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float speed = 15f;
-    public GameObject bullet;
-    public float gravity = 0f;
+    public Rigidbody2D rb;     // The rigidbody component of the prefab
+    public float speed = 15f;  // The speed the player travels at
+    public GameObject bullet;  // The prefab of the bullet (shooty shooty pew pew bang bang)
     
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-
+        rb = GetComponent<Rigidbody2D>(); // Grabs the 2D component of the Rigidbody
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector3(speed * Input.GetAxis("Horizontal"), speed * Input.GetAxis("Vertical"), 0f);
-        if (Input.GetAxis("Fire1") == 1)
+        rb.velocity = new Vector3(speed * Input.GetAxis("Horizontal"), speed * Input.GetAxis("Vertical"), 0f); // Player movement
+
+        if (Input.GetAxis("Fire1") == 1) // Deals with bullet instantiation
         {
-            GameObject clone = Instantiate(bullet, transform.position, Quaternion.identity);
-            var change_input = clone.GetComponent("bulletMover") as bulletMover;
-            change_input.input_position = transform.position;
+            GameObject clone = Instantiate(bullet, transform.position, Quaternion.identity); // Instantiates the bullet
+            var change_input = clone.GetComponent("bulletMover") as bulletMover;             // Grabs the bulletMover script 
+            change_input.input_position = transform.position;                                // and sets the input_position vector to be the player's vector
         }
 
     }
