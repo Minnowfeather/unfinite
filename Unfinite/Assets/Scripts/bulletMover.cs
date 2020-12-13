68c0e209266083ffc1171055c0dfea90b69f1369
Lwizard3 <lukaswfullner2@icloud.com>
2020-12-12 17:26:29 -0800

    Clean n Delay
    I cleaned up some of Connor's code and added a fire delay to the player.

--- Unfinite/Assets/Scripts/bulletMover.cs
+++ Unfinite/Assets/Scripts/bulletMover.cs
@@ -6,22 +6,14 @@ public class bulletMover : MonoBehaviour
 {
     public Rigidbody2D bullet; // The prefab of the bullet
     public Vector3 input_position; // The position of the player when the bullet is fired
+    public float speed = 10f;
 
     void Start()
     {
         bullet = GetComponent<Rigidbody2D>();                                                   // Grabs the 2D Rigidbody component
         Vector3 vec = -(input_position - Camera.main.ScreenToWorldPoint(Input.mousePosition));  // Creates a vector that goes from the player's position to the mouse's position
         vec.z = 0;              // sets the z value of that vector to be zero (we are in 2d here hehe)
+        vec = vec.normalized * speed;
         bullet.velocity = vec;  // sets the bullets velocity equal to that vector (bullet go WEEEEE)     
-    }
-
-    void Update()
-    {
-
-    }
-
-    void FixedUpdate()
-    {
-
     }
 }
