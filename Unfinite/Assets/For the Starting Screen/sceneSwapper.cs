2e16d6795e5148b05d245f8840f4313046ad9c18
Minnowfeather <35814311+Minnowfeather@users.noreply.github.com>
2020-12-13 16:35:43 -0800

    Added scene swapper

---
+++ Unfinite/Assets/For the Starting Screen/sceneSwapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.SceneManagement;
+
+public class sceneSwapper : MonoBehaviour
+{
+    public void swapScene(string scene){
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+}
